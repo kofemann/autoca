e0b435d7d77d02df61bf6cd2935e0bca4b37cbed
Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>
2016-12-28 12:46:28 +0100

    added IPv6 matcher tests

--- webca/ip_filter_test.go
+++ webca/ip_filter_test.go
@@ -20,12 +20,35 @@ var nonMatchingIP4 = []string{
 	"192.168.18.0/24",
 }
 
-func TestIpMatcher(t *testing.T) {
+var matchingIP6 = []string{
+	"fe80::a288:b4ff:fef1:2be8",
+	"fe80::a288:b4ff:fef1:2be8/128",
+	"fe80::a288:b4ff:fef1::1/64",
+}
+
+var nonMatchingIP6 = []string{
+	"fe80::a288:b4ff:fef1:2be1",
+	"1fe80::a288:b4ff:fef1:2be1/128",
+	"fe80::9cef:b4ff:fef1::1/64",
+}
+
+func TestIPv4Matcher(t *testing.T) {
 
 	if !IpMatch(testIPv4, matchingIP4) {
 		t.Error("test failed: matching not detected")
 	}
 	if IpMatch(testIPv4, nonMatchingIP4) {
+		t.Error("test failed: non matcing not detected")
+	}
+
+}
+
+func TestIPv6Matcher(t *testing.T) {
+
+	if !IpMatch(testIPv6, matchingIP6) {
+		t.Error("test failed: matching not detected")
+	}
+	if IpMatch(testIPv6, nonMatchingIP6) {
 		t.Error("test failed: non matcing not detected")
 	}
 
